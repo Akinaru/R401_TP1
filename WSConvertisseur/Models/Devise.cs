35fc59c0b7b233ca9a1d7b96f13ffa89fc3a4cff
gallottm <gallottm@D352-S16>
2024-01-24 14:39:08 +0100

    Ajout modèle

---
+++ WSConvertisseur/Models/Devise.cs
@@ -0,0 +1,37 @@
+﻿namespace WSConvertisseur.Models
+{
+    public class Devise
+    {
+
+        private int id;
+        private string? nomDevise;
+        private double taux;
+
+        public int Id
+        {
+            get { return this.id; }
+            set { this.id = value; }
+        }
+
+        public string? NomDevise
+        {
+            get { return this.nomDevise; }
+            set { this.nomDevise = value; }
+        }
+
+        public double Taux
+        {
+            get { return this.taux; }
+            set { this.taux = value; }
+        }
+
+        public Devise(int id, string? nomDevise, double taux)
+        {
+            Id = id;
+            NomDevise = nomDevise;
+            Taux = taux;
+        }
+
+        public Devise() { }
+    }
+}
